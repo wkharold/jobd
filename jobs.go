221c80fbbf442523856d8458eb254c336529251c
Ward K Harold <wkharold@gmail.com>
2013-11-22 13:04:56 -0600

    More skeleton code to get a clean compile

--- jobs.go
+++ jobs.go
@@ -8,3 +8,11 @@ import (
 type jobsdir struct {
 	srv.File
 }
+
+func (jd jobsdir) Read(fid *srv.FFid, buf []byte, offset uint64) (int, error) {
+	return 0, nil
+}
+
+func (jd *jobsdir) Wstat(fid *srv.FFid, dir *p.Dir) error {
+	return nil
+}
