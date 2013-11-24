516096f019973dc7edd29c76c69f225eb563e686
Ward K Harold <wkharold@gmail.com>
2013-11-23 19:57:06 -0600

    Keep the job definition in the job directory, eliminate redundant file types

--- job.go
+++ job.go
@@ -22,25 +22,21 @@ type jobdef struct {
 	name     string
 	schedule string
 	cmd      string
+	state    string
 }
+
+type jobreader func() []byte
+type jobwriter func([]byte) (int, error)
 
 type job struct {
 	srv.File
+	defn jobdef
 }
 
-type jobctl struct {
+type jobfile struct {
 	srv.File
-	state string
-}
-
-type jobsched struct {
-	srv.File
-	schedule string
-}
-
-type jobcmd struct {
-	srv.File
-	cmd string
+	reader jobreader
+	writer jobwriter
 }
 
 func mkJob(root *srv.File, user p.User, def jobdef) (*job, error) {
@@ -49,21 +45,54 @@ func mkJob(root *srv.File, user p.User, def jobdef) (*job, error) {
 
 	glog.V(3).Infoln("Creating job directory: ", def.name)
 
-	job := &job{}
+	job := &job{defn: def}
 
-	ctl := &jobctl{state: STOPPED}
+	ctl := &jobfile{
+		reader: func() []byte {
+			return []byte(job.defn.state)
+		},
+		writer: func(data []byte) (int, error) {
+			switch cmd := strings.ToLower(string(data)); cmd {
+			case STOP:
+				if job.defn.state != STOPPED {
+					glog.V(3).Infof("Stopping job: %v", job.defn.name)
+					job.defn.state = STOPPED
+				}
+				return len(data), nil
+			case START:
+				if job.defn.state != STARTED {
+					glog.V(3).Infof("Starting job: %v", job.defn.name)
+					job.defn.state = STARTED
+				}
+				return len(data), nil
+			default:
+				return 0, fmt.Errorf("Unknown command: %s", cmd)
+			}
+		}}
 	if err := ctl.Add(&job.File, "ctl", user, nil, 0555, ctl); err != nil {
 		glog.Errorf("Can't create %s/ctl [%v]", def.name, err)
 		return nil, err
 	}
 
-	sched := &jobsched{schedule: def.schedule}
+	sched := &jobfile{
+		reader: func() []byte {
+			return []byte(def.schedule)
+		},
+		writer: func(data []byte) (int, error) {
+			return 0, srv.Eperm
+		}}
 	if err := sched.Add(&job.File, "schedule", user, nil, 0444, sched); err != nil {
 		glog.Errorf("Can't create %s/schedule [%v]", def.name, err)
 		return nil, err
 	}
 
-	cmd := &jobcmd{cmd: def.cmd}
+	cmd := &jobfile{
+		reader: func() []byte {
+			return []byte(def.cmd)
+		},
+		writer: func(data []byte) (int, error) {
+			return 0, srv.Eperm
+		}}
 	if err := cmd.Add(&job.File, "cmd", user, nil, 0444, cmd); err != nil {
 		glog.Errorf("Can't create %s/cmd [%v]", def.name, err)
 		return nil, err
@@ -86,7 +115,7 @@ func mkJobDefinition(name, schedule, cmd string) (*jobdef, error) {
 		return nil, err
 	}
 
-	return &jobdef{name, schedule, cmd}, nil
+	return &jobdef{name, schedule, cmd, STOPPED}, nil
 }
 
 func (j job) Read(fid *srv.FFid, buf []byte, offset uint64) (int, error) {
@@ -103,82 +132,35 @@ func (j *job) Wstat(fid *srv.FFid, dir *p.Dir) error {
 	return nil
 }
 
-func (ctl jobctl) Read(fid *srv.FFid, buf []byte, offset uint64) (int, error) {
-	glog.V(4).Infof("Entering jobctl.Read(%v, %v, %v)", fid, buf, offset)
-	defer glog.V(4).Infof("Exiting jobctl.Read(%v, %v, %v)", fid, buf, offset)
+func (jf jobfile) Read(fid *srv.FFid, buf []byte, offset uint64) (int, error) {
+	glog.V(4).Infof("Entering jofile.Read(%v, %v, %)", fid, buf, offset)
+	defer glog.V(4).Infof("Exiting jobfile.Read(%v, %v, %v)", fid, buf, offset)
 
-	if offset > uint64(len(ctl.state)) {
+	cont := jf.reader()
+
+	if offset > uint64(len(cont)) {
 		return 0, nil
 	}
 
-	copy(buf, ctl.state[offset:])
-	return len(ctl.state[offset:]), nil
+	contout := cont[offset:]
+
+	copy(buf, contout)
+	return len(contout), nil
 }
 
-func (ctl *jobctl) Write(fid *srv.FFid, data []byte, offset uint64) (int, error) {
+func (jf jobfile) Wstat(fid *srv.FFid, dir *p.Dir) error {
+	glog.V(4).Infof("Entering jobfile.Wstat(%v, %v)", fid, dir)
+	defer glog.V(4).Infof("Exiting jobfile.Wstat(%v, %v, %v)", fid, dir)
+
+	return nil
+}
+
+func (jf *jobfile) Write(fid *srv.FFid, data []byte, offset uint64) (int, error) {
 	glog.V(4).Infof("Entering jobctl.Write(%v, %v, %v)", fid, data, offset)
 	defer glog.V(4).Infof("Exiting jobctl.Write(%v, %v, %v)", fid, data, offset)
 
-	ctl.Lock()
-	defer ctl.Lock()
+	jf.Parent.Lock()
+	defer jf.Parent.Lock()
 
-	switch cmd := strings.ToLower(string(data)); cmd {
-	case STOP:
-		if ctl.state != STOPPED {
-			glog.V(3).Infof("Stopping job: %v", ctl.Parent)
-		}
-		return len(data), nil
-	case START:
-		if ctl.state != STARTED {
-			glog.V(3).Infof("Starting job: %v", ctl.Parent)
-		}
-		return len(data), nil
-	default:
-		return 0, fmt.Errorf("Unknown command: %s", cmd)
-	}
-}
-
-func (ctl jobctl) Wstat(fid *srv.FFid, dir *p.Dir) error {
-	glog.V(4).Infof("Entering jobctl.Wstat(%v, %v)", fid, dir)
-	defer glog.V(4).Infof("Exiting jobctl.Wstat(%v, %v)", fid, dir)
-
-	return nil
-}
-
-func (sched jobsched) Read(fid *srv.FFid, buf []byte, offset uint64) (int, error) {
-	glog.V(4).Infof("Entering jobsched.Read(%v, %v, %v)", fid, buf, offset)
-	defer glog.V(4).Infof("Exiting jobsched.Read(%v, %v, %v)", fid, buf, offset)
-
-	if offset > uint64(len(sched.schedule)) {
-		return 0, nil
-	}
-
-	copy(buf, sched.schedule[offset:])
-	return len(sched.schedule[offset:]), nil
-}
-
-func (sched jobsched) Wstat(fid *srv.FFid, dir *p.Dir) error {
-	glog.V(4).Infof("Entering jobsched.Wstat(%v, %v)", fid, dir)
-	defer glog.V(4).Infof("Exiting jobsched.Wstat(%v, %v)", fid, dir)
-
-	return nil
-}
-
-func (cmd jobcmd) Read(fid *srv.FFid, buf []byte, offset uint64) (int, error) {
-	glog.V(4).Infof("Entering jobcmd.Read(%v, %v, %v)", fid, buf, offset)
-	defer glog.V(4).Infof("Exiting jobcmd.Read(%v, %v, %v)", fid, buf, offset)
-
-	if offset > uint64(len(cmd.cmd)) {
-		return 0, nil
-	}
-
-	copy(buf, cmd.cmd[offset:])
-	return len(cmd.cmd[offset:]), nil
-}
-
-func (cmd *jobcmd) Wstat(fid *srv.FFid, dir *p.Dir) error {
-	glog.V(4).Infof("Entering jobcmd.Wstat(%v, %v)", fid, dir)
-	defer glog.V(4).Infof("Exiting jobcmd.Wstat(%v, %v)", fid, dir)
-
-	return nil
+	return jf.writer(data)
 }
